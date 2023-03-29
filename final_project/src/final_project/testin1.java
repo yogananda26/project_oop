83104656d32822aaaad3c0bb107c64b7ae39069f
yogananda26 <122251793+yogananda26@users.noreply.github.com>
2023-03-29 21:45:32 +0700

    Add files via upload

---
+++ final_project/src/final_project/testin1.java
@@ -0,0 +1,7 @@
+package final_project;
+
+public class testin1 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ final_project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bcamaster
+ *
+ */
+module final_project {
+}
