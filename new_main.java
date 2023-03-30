6d80e43200c880a9933611326ef64e7ae3a2f3f3
yogannda <imadeyoga219@gmail.com>
2023-03-30 15:46:37 +0700

    changing_feature

--- new_main.java
+++ new_main.java
@@ -1,6 +1,16 @@
+
 public class new_main{
     public static void main(String[] args) {
         System.out.println("main");
         System.out.println("this is new");   
+    }
+    
+    class testing{
+        // this is for testing the team workflow
+        testing(int input){
+            System.out.println("this is for testing public check public check");
+            System.out.println();
+            System.out.println(input);
+        }
     }
 }
