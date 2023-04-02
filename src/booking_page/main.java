368c9d74000500b1acb2445377cf48bdbb20a383
yogannda <imadeyoga219@gmail.com>
2023-04-02 15:12:38 +0700

    yoga_commit

--- src/booking_page/main.java
+++ src/booking_page/main.java
@@ -3,17 +3,5 @@ package booking_page;
 import javax.swing.event.SwingPropertyChangeSupport;
 
 public class main{
-    public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("testing");
-        System.out.println("tsting");
-    }
-    /**
-     * Innermain
-     */
-    public record Innermain() {
-    }
-    class testing{
-        
-    }
+    
 }
