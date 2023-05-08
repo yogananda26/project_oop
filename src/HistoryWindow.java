c19f73b97e442ed6f3d346a3f5ecffd6370dc378
MarvelJo <mjmarvelm@gmail.com>
2023-05-08 16:38:46 +0700

    new file:   bin/HistoryWindow$1.class 	modified:   bin/HistoryWindow.class 	modified:   src/HistoryWindow.java

--- src/HistoryWindow.java
+++ src/HistoryWindow.java
@@ -4,13 +4,23 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.jar.Attributes.Name;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.Border;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import org.junit.runner.Result;
 
 
 
@@ -18,6 +28,10 @@ public class HistoryWindow extends JFrame implements ActionListener {
 
     JFrame main_frame = new JFrame("History Frame");
     JPanel panel_head = new JPanel(new BorderLayout()); 
+    // JPanel panel_passenger = new JPanel();
+    ArrayList <JButton> passButtons = new ArrayList<>();
+    ;
+    JTable table_history;
 
     private String navigation[] = {
         "Home", "Date", "Profile", "Balance"
@@ -32,45 +46,141 @@ public class HistoryWindow extends JFrame implements ActionListener {
         //this is for frontend thing
         index = idx; 
         try {
-            JLabel label_head = new JLabel("This is your History " + Database.user.get(index).getFullName()); 
+            JLabel label_head = new JLabel("THIS IS YOUR HISTORY " + Database.user.get(index).getFullName()); 
             panel_head.add(label_head, BorderLayout.NORTH);
+
+
+            //column names
+            String[] column_name={"AIRLINE", "PRICE", "DEPARTURE ⇾ ARRIVAL", "DATE", "PASSENGER"};
+            
+            //adding data
             ArrayList <History> temp = Database.user.get(index).history;
-            JPanel panel_history = new JPanel(new GridLayout(temp.size(), 1)); 
-            for(int i = 0; i<temp.size(); i++){
+            String[][] data = new String[temp.size()][5];
+            for (int i = 0; i < temp.size(); i++) {
+                data[i][0] = temp.get(i).get_transportation_name();
+                data[i][1] = "Rp" + temp.get(i).get_price().toString();
+                data[i][2] = temp.get(i).get_departure() + " ⇾ " + temp.get(i).get_arrival();
+                data[i][3] = temp.get(i).get_date();
+                data[i][4] = "Click To See Passengers Detail";
+
                 
-                JLabel label_history = new JLabel(temp.get(i).get_transportation_name());
-                JLabel label_history1 = new JLabel(temp.get(i).get_price().toString());
-                JLabel label_history2 = new JLabel(temp.get(i).get_arrival());
-                JLabel label_history3 = new JLabel(temp.get(i).get_departure());
-                JLabel label_history4 = new JLabel(temp.get(i).get_date());
-
-                panel_history.add(label_history);
-                panel_history.add(label_history1);
-                panel_history.add(label_history2);
-                panel_history.add(label_history3);
-                panel_history.add(label_history4);
-
-
-                ArrayList <Passanger> temp1 = Database.user.get(index).history.get(i).get_array_passanger();
-                JPanel panel_for_passanger = new JPanel(new GridLayout(temp1.size(), 1)); 
-
-                for(int j = 0; j<temp1.size(); j++){  
-                    JLabel label = new JLabel(temp1.get(j).name);
-                    JLabel label1 = new JLabel(temp1.get(j).age.toString());
-                    JLabel label2 = new JLabel(temp1.get(j).passportNum);
-                    JLabel label3 = new JLabel(temp1.get(j).NIN);
-                    JLabel label4 = new JLabel(temp1.get(j).gender);
-
-                    panel_for_passanger.add(label);
-                    panel_for_passanger.add(label1);
-                    panel_for_passanger.add(label2);
-                    panel_for_passanger.add(label3);
-                    panel_for_passanger.add(label4);
-
-                    panel_history.add(panel_for_passanger);
-                }
-                this.add(panel_history, BorderLayout.CENTER); 
             }
+            
+            //initializing table
+            table_history = new JTable(data, column_name);
+            table_history.setRowHeight(15);
+            table_history.setBounds(40, 50, 300, 400);
+
+            //scroll pane
+            JScrollPane panel_table = new JScrollPane(table_history);
+            this.add(panel_table);
+            
+            ListSelectionModel model = table_history.getSelectionModel();
+            model.addListSelectionListener(new ListSelectionListener() {
+                
+                public void valueChanged(ListSelectionEvent e){
+                    if(! model.isSelectionEmpty()){
+                        //get selected row
+                        int selectedRow = model.getMaxSelectionIndex();
+                        ArrayList <Passanger> temp1 = Database.user.get(index).history.get(selectedRow).get_array_passanger();
+                        String lineSep = System.lineSeparator();
+                        StringBuilder results = new StringBuilder();
+                        for (int i = 0; i < temp1.size(); i++) {
+                            String name = temp1.get(i).name;
+                            String gender = temp1.get(i).gender;
+                            String passport = temp1.get(i).passportNum;
+
+                            results.append("Passenger "+ (i+1) + " :").append(lineSep);
+                            results.append("  Name: ").append(name).append(lineSep);
+                            results.append("  Gender: ").append(gender).append(lineSep);
+                            if(i < temp1.size()){
+                                results.append("  Passport: ").append(passport).append(lineSep).append(lineSep);
+                            }
+                            else{
+                                results.append("  Passport: ").append(passport);
+                            }
+                        }
+                        JOptionPane.showMessageDialog(null, results.toString());
+
+                    }
+                }
+            });
+
+            // Border blackline = BorderFactory.createLineBorder(Color.black);
+            // Border empBorder = BorderFactory.createEmptyBorder(25, 25, 25, 25);
+
+            // JLabel label_head = new JLabel("THIS IS YOUR HISTORY " + Database.user.get(index).getFullName()); 
+            // panel_head.add(label_head, BorderLayout.NORTH);
+
+            // ArrayList <History> temp = Database.user.get(index).history;
+            // JPanel panel_history = new JPanel(new GridLayout(temp.size()+1, 1));
+            // panel_history.setBorder(empBorder);
+            // JLabel title_history = new JLabel("AIRLINE"); 
+            // title_history.setBorder(blackline);
+            // JLabel title_history1 = new JLabel("PRICE"); 
+            // title_history1.setBorder(blackline);
+            // JLabel title_history2 = new JLabel("ARRIVAL"); 
+            // title_history2.setBorder(blackline);
+            // JLabel title_history3 = new JLabel("DERPATURE"); 
+            // title_history3.setBorder(blackline);
+            // JLabel title_history4 = new JLabel("DATE"); 
+            // title_history4.setBorder(blackline);
+            // JLabel title_history5 = new JLabel("PASSENGER"); 
+            // title_history5.setBorder(blackline);
+
+            // panel_history.add(title_history);
+            // panel_history.add(title_history1);
+            // panel_history.add(title_history2);
+            // panel_history.add(title_history3);
+            // panel_history.add(title_history4);
+            // panel_history.add(title_history5);
+
+            // for(int i = 0; i<temp.size(); i++){
+                
+            //     JLabel label_history = new JLabel(temp.get(i).get_transportation_name());
+            //     label_history.setBorder(blackline);
+            //     JLabel label_history1 = new JLabel(temp.get(i).get_price().toString());
+            //     label_history1.setBorder(blackline);
+            //     JLabel label_history2 = new JLabel(temp.get(i).get_arrival());
+            //     label_history2.setBorder(blackline);
+            //     JLabel label_history3 = new JLabel(temp.get(i).get_departure());
+            //     label_history3.setBorder(blackline);
+            //     JLabel label_history4 = new JLabel(temp.get(i).get_date());
+            //     label_history4.setBorder(blackline);
+
+            //     panel_history.add(label_history);
+            //     panel_history.add(label_history1);
+            //     panel_history.add(label_history2);
+            //     panel_history.add(label_history3);
+            //     panel_history.add(label_history4);
+
+
+            //     ArrayList <Passanger> temp1 = Database.user.get(index).history.get(i).get_array_passanger();
+            //     // JPanel panel_for_passanger = new JPanel(new GridLayout(temp1.size(), 1)); 
+            //     // JPanel panel_for_passanger = new JPanel(new GridLayout(temp1.size(), 1)); 
+            //     JPanel panel_for_passanger = new JPanel(new GridLayout(temp1.size()*3, 1)); 
+            //     // panel_for_passanger.setBorder(blackline);
+            //     // JPanel panel_for_passanger = new JPanel(); 
+            //     // panel_for_passanger.setLayout(new BoxLayout(panel_for_passanger, BoxLayout.Y_AXIS));
+
+            //     for(int j = 0; j<temp1.size(); j++){  
+            //         JLabel label = new JLabel("Name: " + temp1.get(j).name);
+            //         // JLabel label1 = new JLabel(temp1.get(j).age.toString());
+            //         JLabel label1 = new JLabel("Passport: " + temp1.get(j).passportNum);
+            //         // JLabel label3 = new JLabel(temp1.get(j).NIN);
+            //         JLabel label2 = new JLabel("Gender: " + temp1.get(j).gender);
+
+            //         panel_for_passanger.add(label);
+            //         panel_for_passanger.add(label1);
+            //         panel_for_passanger.add(label2);
+            //         // panel_for_passanger.add(label3);
+            //         // panel_for_passanger.add(label4);
+
+            //         panel_history.add(panel_for_passanger);
+            //     }
+            //     this.add(panel_head);
+            //     this.add(panel_history); 
+            // }
         } catch (IndexOutOfBoundsException e) {
             JOptionPane.showMessageDialog(null,"You Dont Have Any Record Before");
         }
@@ -89,7 +199,6 @@ public class HistoryWindow extends JFrame implements ActionListener {
         this.setVisible(true);
 
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
